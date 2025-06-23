387290117bffbebd6ad306d8b9d0833e43e35722
gouthamr98 <goutham.r@nestgroup.net>
2025-06-23 15:35:19 +0530

    added user and admin details updation popup

--- src/app/Components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/Components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component,ErrorHandler, OnInit,HostListener} from '@angular/core';
+import { Component,ErrorHandler,ViewChild,ElementRef, OnInit,HostListener} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { UserAuthService } from '../../Services/user-auth.service';
@@ -10,19 +10,25 @@ import { LoggerServiceService } from '../../Services/logger-service.service';
 import { interval,Subscription } from 'rxjs';
 import { timer } from 'rxjs';
 import { NotificationTask } from '../../Models/notificationTask';
+import { AbstractControl, FormsModule,ReactiveFormsModule, FormBuilder, FormGroup, ValidationErrors, Validators,AsyncValidatorFn } from '@angular/forms';
+import { debounceTime, first, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink,FormsModule,ReactiveFormsModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent implements OnInit {
+  @ViewChild('settingsModel') settingModel : ElementRef | undefined;
 
   private notificationSubscription?: Subscription;
   notificationDropdownVisible=false;
   notifications:NotificationTask[]=[];
   private readonly JWT_TOKEN = 'JWT_TOKEN';
+  userFormSettings : FormGroup = new FormGroup({});
+  userByid:Users|null=null;
+  formValue: any;
 
   userList:Users[]=[];
   taskLists:Tasks[]=[];
@@ -46,15 +52,16 @@ export class AdminDashboardComponent implements OnInit {
 
   
   
-  constructor(private authService: UserAuthService,private userService:UsersService,private taskService:TasksService,
+  constructor(private authService: UserAuthService,private userService:UsersService,private fb:FormBuilder,private taskService:TasksService,
     private logger:LoggerServiceService,private errorHandler:ErrorHandler) {  }
 
   ngOnInit() {
    
     this.getUser();
     this.getTask();
+    this.setFormStateSettings();
 
-    this.notificationSubscription=timer(10000).subscribe(()=>{this.loadNotification();this.notificationSubscription?.unsubscribe()})
+    this.notificationSubscription=timer(5000).subscribe(()=>{this.loadNotification();this.notificationSubscription?.unsubscribe()})
   }
   ngOnDestroy():void{
     this.notificationSubscription?.unsubscribe();
@@ -166,4 +173,171 @@ getTask(){
     });
 
   }
+
+  getUserInfoFromToken(): {userId:number;roleId:number}|null {
+    const token = sessionStorage.getItem(this.JWT_TOKEN);
+    //const token = localStorage.getItem('token');  // or wherever you store it
+  
+    if (!token) return null;
+  
+    try {
+      // JWT format: header.payload.signature
+      const payloadBase64 = token.split('.')[1];
+      const payloadJson = atob(payloadBase64);
+      const payload = JSON.parse(payloadJson);
+  
+      // Extract the userId from the specific claim
+      const userIdString = payload['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'];
+      const roleName= payload['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+      
+      if (!userIdString||!roleName) return null;
+      const userId=parseInt(userIdString,10)
+      const roleId=this.mapRoleNameToId(roleName)
+  
+      return{userId,roleId};
+  
+    } catch (error) {
+      console.error('Error parsing JWT token:', error);
+      return null;
+    }
+  }
+
+  private mapRoleNameToId(roleName:string):number{
+    switch(roleName.toLowerCase()){
+      case 'admin':return 1;
+      case 'user':return 2;
+      default:return 0;
+    }
+
+  }
+
+  setFormStateSettings()
+  {
+    this.userFormSettings = this.fb.group({
+      id: this.getUserInfoFromToken()?.userId,
+      name: ['',[Validators.required]],
+      userName: ['',[Validators.required,], [this.UsernameExistsValidator()]],
+      email: ['',[Validators.required, Validators.email,
+        // Validators.pattern('^[a-zA-Z0-9._%+-]+@example\\.com$')
+        Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$') 
+        ]],
+      password: ['',[Validators.required]],
+      roleid: this.getUserInfoFromToken()?.roleId,
+      phoneNumber: ['',[Validators.required, Validators.maxLength(10), Validators.minLength(10),Validators.pattern(/^[0-9]{10}$/)]],
+      gender: ['',Validators.required]
+    });
+
+
+  }
+
+  goToSettingsOpenModal() {
+    //debugger
+    const userInfo = this.getUserInfoFromToken();
+    if(!userInfo || userInfo==undefined)
+    {
+      console.error('User ID not found in token');
+      return;
+    }
+  
+    this.userService.getUserbyId(userInfo.userId).subscribe({
+      next: (data) => {
+        debugger
+        this.userByid=data;
+        this.userFormSettings.patchValue({
+          name: this.userByid.name,
+          userName: this.userByid.userName,
+          email: this.userByid.email,
+          password: this.userByid.password, // don't show real password
+          phoneNumber: this.userByid.phoneNumber,
+          gender: this.userByid.gender,
+          role:this.userByid.roleId
+        });
+  
+        const updateUser = document.getElementById('settingsModel');
+        if (updateUser != null) {
+          updateUser.style.display = 'block';
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching user', err);
+        alert('Failed to load user settings.');
+      }
+    });
+  }
+
+
+   closeGotoSettingModal(){
+    this.setFormStateSettings();
+     if(this.settingModel!=null)
+     {
+       this.settingModel.nativeElement.style.display='none';
+     }
+     this.userFormSettings.reset();
+   }
+
+   UsernameExistsValidator(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        debounceTime(300),
+        switchMap(username => this.userService.checkUsernameExists(username)),
+        map(exists => (exists ? { usernameTaken: true } : null)),
+        first()
+      );
+    };
+  }
+
+  updateUser(){
+    debugger
+    console.log(this.userFormSettings.value);
+    if(this.userFormSettings.invalid)
+    {
+      alert('Please Fill All Fields....');
+      return;
+    }
+    this.formValue = this.userFormSettings.value;
+      this.userService.updateUser(this.formValue).subscribe({
+        next: (res) => {
+          alert('User Updated Successfully....');
+          this.logger.info('User updated successfully');
+          this.getUserByid();
+          this.userFormSettings.reset();
+          this.closeGotoSettingModal();
+          window.location.reload();
+        },
+        error: (err) => {
+          console.error('Failed to update user', err);
+          this.logger.error('Failed to update user', err);
+          this.errorHandler.handleError(err);
+          alert('Failed to update user. Please try again later.');
+        }
+      });
+  }
+
+  getUserByid()
+  {
+    const userInfo = this.getUserInfoFromToken();
+    if (!userInfo || userInfo== undefined) {
+      console.warn('User ID not found in token');
+      return;
+    }
+    this.userService.getUserbyId(userInfo.userId).subscribe((res) => {
+    this.userByid = res;
+  })
+  }
+
+  get userNameControl()
+ {
+  return this.userFormSettings.get('userName');
+ } 
+
+ get emailControl() {
+  return this.userFormSettings.get('email');  // Access the email control
+ }
+
+  get phoneNumberControl(){
+    return this.userFormSettings.get('phoneNumber');
+  }
+
+ 
+
 }
