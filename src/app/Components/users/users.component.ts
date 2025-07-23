72bce5f538691a9cdde4c7ca19483c1c6cd2c671
vishnuharidas7 <vishnu.haridas@nestgroup.net>
2025-07-23 15:31:59 +0530

    Added angular material for user account status update

--- src/app/Components/users/users.component.ts
+++ src/app/Components/users/users.component.ts
@@ -6,11 +6,12 @@ import { CommonModule } from '@angular/common';
 import { combineLatest, debounceTime, first, map, switchMap,of } from 'rxjs';
 import { RouterLink} from '@angular/router';
 import { LoggerServiceService } from '../../Services/logger-service.service';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule,RouterLink,FormsModule],
+  imports: [CommonModule, ReactiveFormsModule,RouterLink,FormsModule, MatSlideToggleModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
@@ -25,6 +26,18 @@ userForm : FormGroup = new FormGroup({});
 pswdForm: FormGroup = new FormGroup({});
 private readonly JWT_TOKEN = 'JWT_TOKEN';
 constructor(private fb: FormBuilder,private logger:LoggerServiceService,private errorHandler:ErrorHandler){}
+
+// onStatusToggle(event: any): void {
+//   const isActive = event.checked; // true or false
+//   this.userForm.get('userStatus')?.setValue(isActive ? 1 : 0);
+// }
+
+onStatusToggle(event: any): void {
+  const isActive = event.checked; // already true or false
+  this.userForm.get('isActive')?.setValue(isActive); // ✅ set as boolean
+}
+
+
 formValue: any;
 
 filters = {
@@ -117,6 +130,11 @@ goToPage(page: number): void {
   this.updatePaginatedTasks();
 }
 
+// onStatusToggle(event: MatSlideToggleChange): void {
+//   this.userForm.get('userStatus')?.setValue(event.checked ? 1 : 2);
+// }
+
+
   openModal()
   {
     const userModel = document.getElementById('myModal');
@@ -179,7 +197,8 @@ get userNameControl()
       //password: ['',[Validators.required]],
       roleid: ['',[Validators.required]],
       phoneNumber: ['',[Validators.required, Validators.maxLength(10), Validators.minLength(10),Validators.pattern(/^[0-9]{10}$/)]],
-      gender: ['',Validators.required]
+      gender: ['',Validators.required],
+      isActive:[true]
     });
   }
 
@@ -264,9 +283,12 @@ get userNameControl()
       //User
       {
         ...user,
-        roleid: Number(user.roleId)
+        roleid: Number(user.roleId),
+        isActive: Number(user.status)
       });
   }
+
+  
 
   onDelete(id: number) {
     const isConfirm = confirm("Are you sure you want to delete this User?");
